848673852c49638b7a3797538b10d755049040bc
Matthew-S-Wilson <matthew255@gmail.com>
2023-01-09 10:45:03 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,69 @@
+/*Get a piece of A3 paper
+Fold it in half
+Clean my fountain pen
+Fill fountain pen with ink color I want
+Write letter
+Fold letter to make it into an envelope to be addressed
+Melt wax colors I want for the seal
+Pour wax onto letter
+Press seal into wax
+Address the envelope
+Add stamp to envelope
+Mail letter*/
+
+const getA3Paper = (paper) => {
+    console.log('Get a piece of A3 paper')
+    }
+    
+    const foldPaper = () => {
+    console.log('Fold it in half')
+    }
+    
+    const cleanPen = () => {
+    console.log('Clean my fountain pen')
+    }
+    
+    const fillPen = () => {
+    console.log('Fill fountain pen with ink color I want')
+    }
+    
+    const writeLetter = () => {
+    console.log('Write letter')
+    }
+    
+    const envelopeFold =() => {
+    console.log('Fold letter to make it into an envelope to be addressed')
+    }
+    
+    const meltWax = () => {
+    console.log('Melt wax colors I want for the seal')
+    }
+    
+    const pourWax = () => {
+    console.log('Pour wax onto letter')
+    }
+    
+    const pressSeal = () => {
+    console.log('Press seal into wax')
+    }
+    
+    const addressEnvelope = () => {
+    console.log('Address the envelope')
+    }
+    
+    const addStamp = () => {
+    console.log('Add stamp to envelope')
+    }
+    
+    const mailLetter = () => {
+    console.log('Mail letter')
+    }
+    
+     const Step1 = startOven()
+     const Step2 = gatherIngredients()
+     const Step3 =measureIngredients()
+     createBatter()
+     startBaking()
+     rotateCake()
+     checkIfDone()
+     addFrosting()
